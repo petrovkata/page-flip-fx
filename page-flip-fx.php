9c0cafab6076b2e04d01fb31917c59b3af916120
flashxml <flashxml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-12 12:33:10 +0000

    git-svn-id: https://plugins.svn.wordpress.org/page-flip-fx/trunk@262804 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- page-flip-fx.php
+++ page-flip-fx.php
@@ -2,29 +2,45 @@
 /*
 Plugin Name: Page Flip FX
 Plugin URI: http://www.flashxml.net/page-flip.html
-Description: One of the most advanced Page Flip. Completely XML customizable, without using Flash. And it's free!
-Version: 0.1.6
+Description: Short description:
+One of the most advanced Page Flip. Completely XML customizable, without using Flash. And it's free!
+Version: 0.2.0
 Author: FlashXML.net
 Author URI: http://www.flashxml.net/
 License: GPL2
 */
 
+/* start global parameters */
 	$pageflipfx_params = array(
 		'count'	=> 0, // number of Page Flip FX embeds
 	);
+/* end global parameters */
 
+/* start client side functions */
 	function pageflipfx_get_embed_code($pageflipfx_attributes) {
-		$plugin_dir = basename(dirname(__FILE__));
 		global $pageflipfx_params;
 		$pageflipfx_params['count']++;
 
-		$settings_file_name = !empty($pageflipfx_attributes[2]) ? $pageflipfx_attributes[2] : 'settings.xml';
-		$settings_file_path = WP_PLUGIN_DIR."/{$plugin_dir}/component/$settings_file_name";
+		$plugin_dir = get_option('pageflipfx_path');
+		if ($plugin_dir === false) {
+			$plugin_dir = 'flashxml/page-flip-fx';
+		}
+		$plugin_dir = trim($plugin_dir, '/');
 
-		if (file_exists($settings_file_path)) {
-			$data = simplexml_load_file($settings_file_path);
-			$width = (int)$data->General_Properties->width->attributes()->value;
-			$height = (int)$data->General_Properties->height->attributes()->value;
+		$settings_file_name = !empty($pageflipfx_attributes[2]) ? $pageflipfx_attributes[2] : 'settings.xml';
+		$settings_file_path = WP_CONTENT_DIR . "/{$plugin_dir}/{$settings_file_name}";
+
+		if (function_exists('simplexml_load_file')) {
+			if (file_exists($settings_file_path)) {
+				$data = simplexml_load_file($settings_file_path);
+				$width = (int)$data->General_Properties->width->attributes()->value;
+				$height = (int)$data->General_Properties->height->attributes()->value;
+			}
+		} elseif ((int)$pageflipfx_attributes[4] > 0 && (int)$pageflipfx_attributes[6] > 0) {
+			$width = $pageflipfx_attributes[4];
+			$height = $pageflipfx_attributes[6];
+		} else {
+			return '<!-- invalid Page Flip FX width and / or height -->';
 		}
 
 		if ($width == 0 || $height == 0) {
@@ -34,8 +50,8 @@ License: GPL2
 		$swf_embed = array(
 			'width' => $width,
 			'height' => $height,
-			'text' => trim($pageflipfx_attributes[3]),
-			'component_path' => WP_PLUGIN_URL."/{$plugin_dir}/component/",
+			'text' => trim($pageflipfx_attributes[7]),
+			'component_path' => WP_CONTENT_URL . "/{$plugin_dir}/",
 			'swf_name' => 'pageflipper.swf',
 		);
 		$swf_embed['swf_path'] = $swf_embed['component_path'].$swf_embed['swf_name'];
@@ -64,18 +80,59 @@ License: GPL2
 	}
 
 	function pageflipfx_filter_content($content) {
-		return preg_replace_callback('|\[page-flip-fx\s*(settings="([^\]]+)")?\s*\](.*)\[/page-flip-fx\]|i', 'pageflipfx_get_embed_code', $content);
+		return preg_replace_callback('|\[page-flip-fx\s*(settings="([^"]+)")?\s*(width="([0-9]+)")?\s*(height="([0-9]+)")?\s*\](.*)\[/page-flip-fx\]|i', 'pageflipfx_get_embed_code', $content);
 	}
 
-	function pageflipfx_echo_embed_code($settings_xml_path = '', $div_text = '') {
-		echo pageflipfx_get_embed_code(array(2 => $settings_xml_path, 3 => $div_text));
+	function pageflipfx_echo_embed_code($settings_xml_path = '', $div_text = '', $width = 0, $height = 0) {
+		echo pageflipfx_get_embed_code(array(2 => $settings_xml_path, 3 => $div_text, 4 => $width, 6 => $height));
 	}
 
 	function pageflipfx_load_swfobject_lib() {
 		wp_enqueue_script('swfobject');
 	}
+/* end client side functions */
 
+/* start admin section functions */
+	function pageflipfx_admin_menu() {
+		add_options_page('Page Flip FX Options', 'Page Flip FX', 'manage_options', 'pageflipfx', 'pageflipfx_admin_options');
+	}
+
+	function pageflipfx_admin_options() {
+		  if (!current_user_can('manage_options'))  {
+	    wp_die(__('You do not have sufficient permissions to access this page.'));
+	  }
+
+	  $pageflipfx_default_path = get_option('pageflipfx_path');
+	  if ($pageflipfx_default_path === false) {
+	  	$pageflipfx_default_path = 'flashxml/page-flip-fx';
+	  }
+?>
+<div class="wrap">
+	<h2>Page Flip FX</h2>
+	<form method="post" action="options.php">
+		<?php wp_nonce_field('update-options'); ?>
+
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row" style="width: 40em;">SWF and assets path is <?php echo WP_CONTENT_DIR; ?>/</th>
+				<td><input type="text" style="width: 25em;" name="pageflipfx_path" value="<?php echo $pageflipfx_default_path; ?>" /></td>
+			</tr>
+		</table>
+		<input type="hidden" name="action" value="update" />
+		<input type="hidden" name="page_options" value="pageflipfx_path" />
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+	</form>
+</div>
+<?php
+	}
+/* end admin section functions */
+
+/* start hooks */
 	add_filter('the_content', 'pageflipfx_filter_content');
 	add_action('init', 'pageflipfx_load_swfobject_lib');
+	add_action('admin_menu', 'pageflipfx_admin_menu');
+/* end hooks */
 
 ?>
--- readme.txt
+++ readme.txt
@@ -15,22 +15,29 @@ You can integrate it in any website for free without even using Flash. It's a fu
 
 == Installation ==
 
-Make sure your Wordpress version is equal or greater than 2.8 and your hosting provider is using PHP5.
+Make sure your Wordpress version is greater than 2.8 and your hosting provider is using PHP5.
 
-1. Upload the `page-flip-fx` folder along with all its subfolders and files to the `/wp-content/plugins/` folder
-2. Activate the plugin from the **Plugins** menu in WordPress Dashboard
-3. In the post editor use the following tag to embed the Page Flip: `[page-flip-fx][/page-flip-fx]`. Or add `<?php pageflipfx_echo_embed_code(); ?>` in your templates
-4. Go to [FlashXML.net](http://www.flashxml.net/ "Free Flash Components") and [customize your Page Flip](http://www.flashxml.net/page-flip.html "Page Flip") using the Live Demo. Generate the `settings.xml` text and use it to overwrite `page-flip-fx/component/settings.xml`
-5. To use your own images, upload them to the `page-flip-fx/component/images` folder and update the `page-flip-fx/component/images.xml` file accordingly
+1. [Download](http://www.flashxml.net/free/download/page-flip.zip "Page Flip FX") or [purchase](http://www.flashxml.net/page-flip.html#swmi-license "Page Flip FX") the Page Flip FX Flash component
+2. Create a new folder inside your `/wp-content/` directory called `flashxml/page-flip-fx` and copy the content of the archive to this folder
+3. Install [the plugin](http://downloads.wordpress.org/plugin/page-flip-fx.zip "Page Flip FX Plugin") or upload the `page-flip-fx` folder along with all its files to `/wp-content/plugins/` directory
+4. Activate the plugin from the **Plugins** tab in **WordPress Dashboard**
+5. Go to **Page Flip FX** from the **Settings** tab and update the path in case you used a different one
+6. In the post editor use the following tag to embed the Page Flip FX: `[page-flip-fx][/page-flip-fx]`. You could also add `<?php pageflipfx_echo_embed_code(); ?>` in the PHP file of your theme
+7. Go to [FlashXML.net](http://www.flashxml.net/ "Free Flash Components") and [customize your Page Flip FX](http://www.flashxml.net/page-flip.html "Page Flip FX") using the Live Demo. Generate the `settings.xml` text and use it to overwrite `flashxml/pageflipfx/settings.xml`
+8. To use your own images, upload them to the `flashxml/pageflipfx/images` folder and update the `flashxml/pageflipfx/images.xml` file accordingly
 
 = Additional settings file =
 
-To embed the Page Flip more than once, you will need another settings file and (probably) another set of images. Let's assume your new file is called **settings2.xml**. From the post editor, use the following code: `[page-flip-fx settings="settings2.xml"][/page-flip-fx]`. From the PHP files of your theme, add the file name as *the first argument* of the `pageflipfx_echo_embed_code()` function call. If you use a separate set of images, don't forget to create a new XML file for that and update the `imagesXML` value in the settings file.
+To embed the Page Flip FX more than once, you will need another settings file and (probably) another set of images. Let's assume your new file is called **settings2.xml**. From the post editor, use the following code: `[page-flip-fx settings="settings2.xml"][/page-flip-fx]`. From the PHP files of your theme, add the file name as *the first argument* of the `pageflipfx_echo_embed_code()` function call. If you use a separate set of images, don't forget to create a new XML file for that and update the value in the settings file.
 
 = No Flash support text =
 
 To support visitors without Adobe Flash, you can provide alternative textual content. From the post editor, add the text between `[page-flip-fx]` and `[/page-flip-fx]`. From the PHP files of your theme, add the text as *the second argument* of the `pageflipfx_echo_embed_code()` function call.
 
+= If you have PHP4 =
+
+To make it work if you're using PHP4, add the following code `[page-flip-fx width="600" height="300"][/page-flip-fx]` in the post editor. From the PHP files of your theme, add the width and height as *the third and fourth argument* of the `pageflipfx_echo_embed_code()` function call. Don't forget to provide your own width and height values, since 600 and 300 are just examples.
+
 == Screenshots ==
 
-1. The Live Demo on [FlashXML.net](http://www.flashxml.net/page-flip.html "Page Flip") is the utility that helps easily customize your Page Flip to fit all of your needs.
+1. The Live Demo on [FlashXML.net](http://www.flashxml.net/page-flip.html "Page Flip FX") is the utility that helps easily customize your Page Flip FX to fit all your needs.
