4bb41f06a36c1022a35fe7f58d49e905727b612a
flashxml <flashxml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-27 12:28:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/page-flip-fx/trunk@304930 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- page-flip-fx.php
+++ page-flip-fx.php
@@ -79,7 +79,7 @@ License: GPL2
 	}
 
 	function pageflipfx_echo_embed_code($settings_xml_path = '', $div_text = '', $width = 0, $height = 0) {
-		echo pageflipfx_get_embed_code(array(2 => $settings_xml_path, 3 => $div_text, 4 => $width, 6 => $height));
+		echo pageflipfx_get_embed_code(array(2 => $settings_xml_path, 7 => $div_text, 4 => $width, 6 => $height));
 	}
 
 	function pageflipfx_load_swfobject_lib() {
